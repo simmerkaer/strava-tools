ccba58b6539a7f6a4276fd4f9ce9c4de75db2bd0
Steffen Immerkær <sim@schultz.dk>
2021-10-23 19:48:56 +0200

    New scaling and cropping implemented

--- Program.cs
+++ Program.cs
@@ -27,37 +27,10 @@ namespace StravaTools
 
             var rawCoordinates = ExtractRawCoordinates(session);
 
-            var allX = rawCoordinates.Select(coordinate => (int)coordinate.X).ToList();
-            var allY = rawCoordinates.Select(coordinate => (int)coordinate.Y).ToList();
+            var shiftedCoordinates = ShiftCoordinatesToOrigin(rawCoordinates);
+            var scaledCoordinates = ScaleCoordinatesToImageSize(shiftedCoordinates);
 
-            var (minX, minY) = new PointF(allX.Min(), allY.Min());
-            var (maxX, maxY) = new PointF(allX.Max(), allY.Max());
-
-            var scale = Math.Sqrt(Math.Pow(maxX - minX, 2) + Math.Pow(maxY - minY, 2));
-            //var scale = ImageScale / rawScale;
-
-            var normalizedCoordinates = rawCoordinates.Select(rawCoordinate =>
-            {
-                var x = ((rawCoordinate.X - minX) / scale) * 1000;
-                var y = ((rawCoordinate.Y - minY) / scale) * 1000;
-                return new PointF((int)x, (int)y);
-            });
-
-            var maxNormalizedX = normalizedCoordinates.Select(x => x.X).Max();
-            var maxNormalizedY = normalizedCoordinates.Select(x => x.Y).Max();
-            var maxAll = Math.Max(maxNormalizedX, maxNormalizedY);
-            bool maxIsX = (int)maxAll == (int)maxNormalizedX;
-            var newScale = 1000 / maxAll;
-
-            var scaledCoordinates = normalizedCoordinates.Select(normalizedCoordinates =>
-            {
-                var x = normalizedCoordinates.X * newScale;
-                var y = normalizedCoordinates.Y * newScale;
-                return new PointF(x, 1000 - y);
-            }).ToList();
-
-            var minYScaled = scaledCoordinates.Select(scaledCoordinate => scaledCoordinate.Y).Min();
-            var minXScaled = scaledCoordinates.Select(scaledCoordinate => scaledCoordinate.X).Min();
+            var cutTop = scaledCoordinates.Select(coordinate => coordinate.Y).Min() > 1;
 
             using var image = new Image<Rgba32>(ImageWidth, ImageHeight);
             PathBuilder pathBuilder = new PathBuilder();
@@ -69,15 +42,60 @@ namespace StravaTools
             image.Mutate<Rgba32>(ctx => ctx
                 .Fill(Color.White) // white background image
                 .Draw(Color.Gray, 3, path)); // draw the path so we can see what the text is supposed to be following
-            if (maxIsX)
+            if (cutTop)
             {
-                image.Mutate(x => x.Crop(new Rectangle(0, (int)minYScaled, 1000, 1000 - (int)minYScaled)));
+                var minY = scaledCoordinates.Select(coordinate => coordinate.Y).Min();
+                image.Mutate(x => x.Crop(new Rectangle(0, (int)minY, 1000, 1000 - (int)minY)));
             }
             else
             {
-                image.Mutate(x => x.Crop(new Rectangle((int)minXScaled, 0, 1000 - (int)minXScaled, 1000)));
+                //image.Mutate(x => x.Crop(new Rectangle((int)minXScaled, 0, 1000 - (int)minXScaled, 1000)));
             }
             image.Save("C:/code/InnovationDay/StravaTools/strava_tools.png");
+        }
+
+        private static List<PointF> ScaleCoordinatesToImageSize(List<PointF> shiftedCoordinates)
+        {
+            var allX = shiftedCoordinates.Select(coordinate => coordinate.X).ToList();
+            var allY = shiftedCoordinates.Select(coordinate => coordinate.Y).ToList();
+
+            var (maxX, maxY) = new PointF(allX.Max(), allY.Max());
+
+            var xDiff = maxX - ImageWidth;
+            var yDiff = maxY - ImageHeight;
+
+            var biggestDiff = Math.Max(xDiff, yDiff);
+            var xIsBiggestDiff = Math.Abs(biggestDiff - xDiff) < 0.001;
+
+            float maybeScale;
+            if (xIsBiggestDiff)
+            {
+                maybeScale = (ImageWidth / xDiff);
+            }
+            else
+            {
+                maybeScale = (ImageWidth / yDiff);
+            }
+
+            var normalizedCoordinates = shiftedCoordinates.Select(rawCoordinate =>
+            {
+                var x = rawCoordinate.X * maybeScale;
+                var y = rawCoordinate.Y * maybeScale;
+                return new PointF(x, 1000 - y);
+            }).ToList();
+            return normalizedCoordinates;
+        }
+
+        private static List<PointF> ShiftCoordinatesToOrigin(List<PointF> rawCoordinates)
+        {
+            var allX = rawCoordinates.Select(coordinate => coordinate.X).ToList();
+            var allY = rawCoordinates.Select(coordinate => coordinate.Y).ToList();
+
+            var (minX, minY) = new PointF(allX.Min(), allY.Min());
+
+            var shiftedCoordinates = rawCoordinates.Select(rawCoordinate =>
+                new PointF(rawCoordinate.X - minX, rawCoordinate.Y - minY)).ToList();
+            return shiftedCoordinates;
         }
 
         private static List<PointF> ExtractRawCoordinates(SessionMessages session)
@@ -90,9 +108,6 @@ namespace StravaTools
                 var positionLong = record.GetFieldValue("PositionLong");
 
                 if (positionLong is null || positionLat is null) return;
-
-                Console.WriteLine("fit-coordinates: {0},{1}", positionLat, positionLong);
-                Console.WriteLine();
 
                 var rawCoordinate = new PointF((int)positionLong, (int)positionLat);
                 rawCoordinates.Add(rawCoordinate);
